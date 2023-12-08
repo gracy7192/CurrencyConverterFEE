2e66813f8629e38a53f2d190c79fbb0e86a8650b
harshit391 <harshitsingla1761@gmail.com>
2023-12-08 19:06:32 +0530

    Hopefully It get fixed

--- static/js/cc.js
+++ static/js/cc.js
@@ -1,50 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // When the DOM content is fully loaded, add a submit event listener to the form
-    var form = document.querySelector('form');
+    var form = document.querySelector('#currencyForm'); // Select the form by its ID
+
     form.addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission behavior
 
-        // Get user input values
-        var amount = parseFloat($('#amount').val());
-      var fromCurrency = $('#selector').val();
-      var toCurrency = $('#selector1').val();
-
+        var amount = parseFloat(document.querySelector('#amount').value);
+        var fromCurrency = document.querySelector('#selector').value;
+        var toCurrency = document.querySelector('#selector1').value;
 
         if (isNaN(amount)) {
-            // Validate if the entered amount is a valid number
             alert('Please enter a valid amount.');
             return;
         }
 
-        // Perform the currency conversion using Open Exchange Rates API
         convertCurrency(amount, fromCurrency, toCurrency);
     });
 });
 
 function convertCurrency(amount, fromCurrency, toCurrency) {
-    var appId = 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_OMcZj2aZob1sVZL9UZPhzOWcJQ3VEh7GyDTySd6A'; // Replace with your actual App ID
+    var appId = 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_OMcZj2aZob1sVZL9UZPhzOWcJQ3VEh7GyDTySd6A';
 
-    // Make a GET request to fetch the latest exchange rates using the API
-    fetch(`https://open.er-api.com/v6/latest/${fromCurrency}?app_id=${appId}`)
-        .then(response => response.json()) // Parse the response as JSON
+    fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`)
+        .then(response => response.json())
         .then(data => {
             if (data.error) {
-                // Check for errors in the API response
                 alert('Failed to fetch exchange rates. Please try again.');
                 return;
             }
 
-            var exchangeRates = data.rates; // Extract exchange rates from API response
+            var exchangeRates = data.rates;
 
-            // Check if the target currency is valid
             if (!exchangeRates[toCurrency]) {
                 alert('Invalid currency selection.');
                 return;
             }
 
-            // Perform the currency conversion
             var convertedAmount = (amount / exchangeRates[fromCurrency]) * exchangeRates[toCurrency];
-            displayResult(convertedAmount.toFixed(2)); // Limit result to 2 decimal places
+            displayResult(convertedAmount.toFixed(2));
         })
         .catch(error => {
             console.error('Error fetching data:', error);
@@ -53,11 +45,9 @@ function convertCurrency(amount, fromCurrency, toCurrency) {
 }
 
 function displayResult(convertedAmount) {
-    // Update the result displayed on the webpage
     var resultElement = document.querySelector('.finalValue');
     resultElement.textContent = convertedAmount;
 
-    // Show the result container
     var resultContainer = document.getElementById('finalAmount');
     resultContainer.style.display = 'block';
 }
